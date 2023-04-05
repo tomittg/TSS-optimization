ade9152f5dd849b16c9e5df31f84f96044ca1e4a
estherlozanoromero <55650862+estherlozanoromero@users.noreply.github.com>
2023-04-05 18:14:00 +0200

    experiments

---
+++ experiments/difusioLT.hh
@@ -0,0 +1,11 @@
+#ifndef DIFUSIOLT_HH
+#define DIFUSIOLT_HH
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int LT(set<int> minimumS);
+
+#endif
---
+++ experiments/main.hh
@@ -0,0 +1,12 @@
+#ifndef MAIN_HH
+#define MAIN_HH
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+extern double r;
+extern vector<vector<int>> graph; 
+
+#endif
